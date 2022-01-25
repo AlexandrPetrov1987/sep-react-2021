98da49b3d5209732846fa1a27d3ee06624cf3109
alex <AlexandrPetrov1987>
2022-01-25 14:00:58 +0200

    change color

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>Hello react</h1>
-            <h1>Hello react</h1>
-            <h1>Hello react</h1>
-            <h1>Hello react</h1>
-            <h1>Hello react</h1>
+            <h1>hello react</h1>
         </div>
     );
 }
