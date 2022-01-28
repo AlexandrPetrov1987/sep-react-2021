bca81cb647eeac4da4dcebc381c009d3baed62b6
alex <AlexandrPetrov1987>
2022-01-28 15:27:20 +0200

    add h1

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>hello react</h1>
-            <h1>hello react</h1>
-            <h1>hello react</h1>
-            <h1>hello react</h1>
-            <h1>hello react</h1>
-            <h1>hello react</h1>
-            <h1>hello react</h1>
-            <h1>hello react</h1>
-
+<h1>Hello</h1>
         </div>
     );
 }
