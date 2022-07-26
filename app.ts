e11e7214d15017dee19c6d3baca982378cb7923a
alex <AlexandrPetrov1987>
2022-07-26 13:09:45 +0300

    add tsc

---
+++ app.ts
@@ -0,0 +1,11 @@
+class User {
+    constructor(public id: number, private name: string, public age: number) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+}
+
+const user = new User(12,'Vasya',13);
+
+console.log(user);
