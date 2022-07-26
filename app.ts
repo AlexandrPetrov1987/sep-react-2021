0db77ffd168fe5df1362579aaf8073d6055e54ba
alex <AlexandrPetrov1987>
2022-07-26 15:32:00 +0300

    add

--- app.ts
+++ app.ts
@@ -1,13 +1,64 @@
-class User {
-    constructor(public id: number, private name: string, public age: number) {
-        this.id = id;
-        this.age = age;
-        this.name = name;
+// 1) створити інтерфейс на основі цього объекта:
+interface ICore   {
+    flight: number,
+    core: {
+        reuse_count: number,
+        status: string,
     }
 }
 
-const user = new User(12,'Vasya',13);
+interface IPayload{
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number,
+}
 
-console.log(user);
+interface ISpace {
+    mission_name: string,
+    launch_date_local: string,
+    launch_site: {
+        site_name_long: string,
+    },
+    links: {
+        article_link: string,
+        video_link: string,
+    },
+    rocket: {
+        rocket_name: string,
+        first_stage: {
+            cores: ICore[]
+        },
+        second_stage: {
+            payloads: IPayload[]
+        }
 
+    }
+}
+// ---------------------------------------------------------------------------—————————
+// 2) протипізувати функці
+interface IUser{
+    name:string,
+    age:number,
+    gender:string,
+}
+const user:IUser = {
+    name:'Max',
+    age:18,
+    gender:'male',
+}
+
+const sum = (a: number, b: number): number => a + b;
+
+const showSum = (a: number, b: number): void => {
+    console.log(a + b);
+};
+
+const incAge = (someUser: IUser, inc:number): IUser => {
+    someUser.age+=inc;
+    return someUser;
+};
+
+console.log(sum(1, 2));
+showSum(2,3)
+incAge(user, 2)
 
